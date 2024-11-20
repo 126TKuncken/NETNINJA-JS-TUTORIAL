70fe89c81561b359bcd7a91663da1ef4ee95e7bf
Tyler Kuncken <126TKuncken@frhsd.com>
2024-11-20 12:32:17 -0500

    video 4

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 //second video
 //Get Elements By Class or Tag" video, the instructor explains how to use getElementsByClassName() and getElementsByTagName() methods to select multiple HTML elements by their class name or tag name for manipulation with JavaScript.
 
-
+//third video
+//shows how to use the querySelector() and querySelectorAll() methods to select HTML elements using CSS selectors for more flexible and powerful element targeting in JavaScript
 
 
